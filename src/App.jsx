760f3fe227d8b415a878fbd162369faf41c46bc3
leo-jng <lucientiel@protonmail.com>
2024-10-03 16:14:08 -0400

    Set up Docker and initial components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,31 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form>
+        <fieldset id="personal_details">
+          <legend> Personal Details </legend>
+          <label for="first_name">First name:</label> <br/>
+          <input type="text" id="first_name" name="first_name" placeholder="John" /> <br/>
+
+          <label for="last_name">Last name:</label> <br/>
+          <input type="text" id="last_name" name="last_name" placeholder="Doe"/> <br/>
+
+          <label for="phone_number">Phone Number:</label> <br/>
+          <input type="tel" id="phone_number" name="phone_number" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder="000-000-0000"></input> <br/>
+ 
+          <label for="email_address">Email:</label> <br/>
+          <input type="email" id="email_address" name="email_address"></input> <br/>
+
+          <label for= "linkedin_profile">Linkedin:</label> <br/>
+          <input type="url" id="linkedin_url" name="linkedin_url"></input> <br/>
+
+          <label for="personal_github">Github:</label> <br/>
+          <input type="url" id="personal_github" name="personal_github"></input> <br/>
+
+          <label for="personal_website">Your Website:</label> <br/>
+          <input type="url" id="personal_website" name="personal_website"></input> <br/>
+          </fieldset>
+      </form>
     </>
   )
 }
---
+++ src/Components/Experience.jsx
@@ -0,0 +1,12 @@
+
+export default function Experience() {
+    return  (
+        <>
+            <form>
+                <fieldset id="experience">
+                    <legend> Experience</legend>
+                </fieldset>
+            </form>
+        </>
+    )
+}
