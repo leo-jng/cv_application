3db4bfb976b4205a9c086f8c2cf142214cc136fa
leo-jng <lucientiel@protonmail.com>
2024-11-28 23:10:03 -0500

    Initialized Experience Component

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,12 +1,15 @@
-
+// This Experience Component contains the inputs that will affect the
+// data held in the Experience state object.
+// This includes: Role, Company Name, Date Span of Employment
+//                Input in STAR (SITUATION, TASK, ACTION, RESULT) for each job
 export default function Experience() {
-    return  (
-        <>
-            <form>
-                <fieldset id="experience">
-                    <legend> Experience</legend>
-                </fieldset>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <form>
+        <fieldset id="experience">
+          <legend> Experience</legend>
+        </fieldset>
+      </form>
+    </>
+  );
 }
