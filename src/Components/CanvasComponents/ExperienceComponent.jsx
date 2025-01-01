5db4ec0e1a3df9e06c9bf47e2dd9fbd8be95b888
leo-jng <lucientiel@protonmail.com>
2024-12-31 21:00:14 -0500

    Implemented scaling text window for experience component

--- src/Components/CanvasComponents/ExperienceComponent.jsx
+++ src/Components/CanvasComponents/ExperienceComponent.jsx
@@ -18,18 +18,48 @@ export default function ExperienceComponent({
     console.log("transform/scaling activated");
     const transformingShape = e.target;
     const transform = transformingShape.getTransform().copy();
-    const attrs = transform.decompose(); // decompose is a prototype method that extracts the transforming attributes into a readable object
+    const transformAttrs = transform.decompose(); // decompose is a prototype method that extracts the transforming attributes into a readable object
     // check what the attributes are
-    console.log("transform:", transform, "attrs:", attrs);
-    const newAttrs = {
-      width: Math.max(110 * attrs.scaleX, 110),
-      height: Math.max(110 * attrs.scaleY, 110),
-      scaleX: 1,
-      scaleY: 1,
-    };
+    console.log("transform:", transform, "attrs:", transformAttrs);
+    // const newAttrs = {
+    //   width: Math.max(110 * transformAttrs.scaleX, 110),
+    //   height: Math.max(110 * transformAttrs.scaleY, 110),
+    //   scaleX: 1,
+    //   scaleY: 1,
+    // };
     // apply the transforming attributes of the transforming shape to the group
-    groupRef.current.setAttrs(newAttrs);
+    // groupRef.current.setAttrs(newAttrs);
     // it is possible to manually iterate through the components held within the group to apply the changes to them instead of the group
+    // console.log("current group", groupRef.current);
+    const groupRefChildren = groupRef.current.getChildren();
+    console.log("children of current group", groupRefChildren);
+    let totalLines = 0; // used to keep track of offsets
+    groupRefChildren.forEach((node) => {
+      // console.log(
+      //   "current width: ",
+      //   node.attrs.width,
+      //   "current scalex: ",
+      //   transformAttrs.scaleX,
+      //   "current transforming attributes: ",
+      //   node.attrs.width * transformAttrs.scaleX
+      // );
+      // if (node.attrs.id == "component_text") {
+      node.setAttrs({
+        width: Math.max(200 * transformAttrs.scaleX, 10),
+        // height: Math.max(20 * transformAttrs.scaleY, 10),
+        scaleX: 1,
+        scaleY: 1,
+        offsetY: -10 * totalLines * transformAttrs.scaleY,
+      });
+
+      const currentLinesCount = node.textArr.length;
+      totalLines += currentLinesCount;
+      console.log(totalLines);
+      // }
+    });
+    groupRef.current.scaleX(1);
+    groupRef.current.scaleY(1);
+    groupRef.current.getLayer().batchDraw();
   }
 
   useEffect(() => {
@@ -50,6 +80,22 @@ export default function ExperienceComponent({
     setSelectedCanvasComponent(CompKey);
   };
 
+  const text0 = onCanvasComponents[CompKey].jobposition;
+  const text1 =
+    onCanvasComponents[CompKey].companyname +
+    ", " +
+    onCanvasComponents[CompKey].companylocation;
+  const text2 = onCanvasComponents[CompKey].employmenttype;
+  const text3 =
+    onCanvasComponents[CompKey].startdate +
+    " to " +
+    onCanvasComponents[CompKey].enddate;
+  const text4 = onCanvasComponents[CompKey].jobachievement_no1;
+  const text5 = onCanvasComponents[CompKey].jobachievement_no2;
+  const text6 = onCanvasComponents[CompKey].jobachievement_no3;
+  const text7 = onCanvasComponents[CompKey].jobachievement_no4;
+
+  // try to rewrite this section in a more smarter way ie without manually typing in each compoent and its changes
   return (
     <>
       <Group
@@ -85,66 +131,122 @@ export default function ExperienceComponent({
           // this is the select border
           stroke={isSelected ? "green" : ""}
           ref={shapeRef}
-          width={70}
-          height={80}
+          width={200}
+          height={110}
         />
         <Group ref={groupRef}>
-          <Rect fill="gray" width={50} height={60} />
+          {/* <Rect id="background_block" fill="gray" width={200} height={110} /> */}
           <Text
-            text={onCanvasComponents[CompKey].jobposition}
+            id="component_text"
+            text={text0}
             fontFamily={onCanvasComponents[CompKey].fontfamily}
             fontSize={onCanvasComponents[CompKey].fontsize}
+            width={200}
+            offsetY={0}
+            // height={20}
           />
           <Text
-            text={
-              onCanvasComponents[CompKey].companyname +
-              ", " +
-              onCanvasComponents[CompKey].companylocation
+            id="component_text"
+            text={text1}
+            fontFamily={onCanvasComponents[CompKey].fontfamily}
+            fontSize={onCanvasComponents[CompKey].fontsize}
+            width={200}
+            // height={20}
+            offsetY={-10 * Math.ceil((text0.length * 6) / 200)}
+          />
+          <Text
+            id="component_text"
+            text={text2}
+            fontFamily={onCanvasComponents[CompKey].fontfamily}
+            fontSize={onCanvasComponents[CompKey].fontsize}
+            width={200}
+            // height={20}
+            offsetY={
+              -10 *
+              (Math.ceil((text0.length * 6) / 200) +
+                Math.ceil((text1.length * 6) / 200))
             }
-            fontFamily={onCanvasComponents[CompKey].fontfamily}
-            fontSize={onCanvasComponents[CompKey].fontsize}
-            offsetY={-10}
           />
           <Text
-            text={onCanvasComponents[CompKey].employmenttype}
+            id="component_text"
+            text={text3}
             fontFamily={onCanvasComponents[CompKey].fontfamily}
             fontSize={onCanvasComponents[CompKey].fontsize}
-            offsetY={-20}
-          />
-          <Text
-            text={
-              onCanvasComponents[CompKey].startdate +
-              " to " +
-              onCanvasComponents[CompKey].enddate
+            width={200}
+            // height={20}
+            offsetY={
+              -10 *
+              (Math.ceil((text0.length * 6) / 200) +
+                Math.ceil((text1.length * 6) / 200) +
+                Math.ceil((text2.length * 6) / 200))
             }
-            fontFamily={onCanvasComponents[CompKey].fontfamily}
-            fontSize={onCanvasComponents[CompKey].fontsize}
-            offsetY={-30}
           />
           <Text
-            text={onCanvasComponents[CompKey].jobachievement_no1}
+            id="component_text"
+            text={text4}
             fontFamily={onCanvasComponents[CompKey].fontfamily}
             fontSize={onCanvasComponents[CompKey].fontsize}
             // width={100}
-            offsetY={-40}
+            width={200}
+            // height={20}
+            offsetY={
+              -10 *
+              (Math.ceil((text0.length * 6) / 200) +
+                Math.ceil((text1.length * 6) / 200) +
+                Math.ceil((text2.length * 6) / 200) +
+                Math.ceil((text3.length * 6) / 200))
+            }
           />
           <Text
-            text={onCanvasComponents[CompKey].jobachievement_no2}
+            id="component_text"
+            text={text5}
             fontFamily={onCanvasComponents[CompKey].fontfamily}
             fontSize={onCanvasComponents[CompKey].fontsize}
-            offsetY={-50}
+            width={200}
+            // height={20}
+            offsetY={
+              -10 *
+              (Math.ceil((text0.length * 6) / 200) +
+                Math.ceil((text1.length * 6) / 200) +
+                Math.ceil((text2.length * 6) / 200) +
+                Math.ceil((text3.length * 6) / 200) +
+                Math.ceil((text4.length * 6) / 200))
+            }
           />
           <Text
-            text={onCanvasComponents[CompKey].jobachievement_no3}
+            id="component_text"
+            text={text6}
             fontFamily={onCanvasComponents[CompKey].fontfamily}
             fontSize={onCanvasComponents[CompKey].fontsize}
-            offsetY={-60}
+            width={200}
+            // height={20}
+            offsetY={
+              -10 *
+              (Math.ceil((text0.length * 6) / 200) +
+                Math.ceil((text1.length * 6) / 200) +
+                Math.ceil((text2.length * 6) / 200) +
+                Math.ceil((text3.length * 6) / 200) +
+                Math.ceil((text4.length * 6) / 200) +
+                Math.ceil((text5.length * 6) / 200))
+            }
           />
           <Text
-            text={onCanvasComponents[CompKey].jobachievement_no4}
+            id="component_text"
+            text={text7}
             fontFamily={onCanvasComponents[CompKey].fontfamily}
             fontSize={onCanvasComponents[CompKey].fontsize}
-            offsetY={-70}
+            width={200}
+            // height={20}
+            offsetY={
+              -10 *
+              (Math.ceil((text0.length * 6) / 200) +
+                Math.ceil((text1.length * 6) / 200) +
+                Math.ceil((text2.length * 6) / 200) +
+                Math.ceil((text3.length * 6) / 200) +
+                Math.ceil((text4.length * 6) / 200) +
+                Math.ceil((text5.length * 6) / 200) +
+                Math.ceil((text6.length * 6) / 200))
+            }
           />
         </Group>
       </Group>
